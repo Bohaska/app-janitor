459bc4d143b6e26c81b8006aec6a04d34e37918a
Bohaska <73286691+Bohaska@users.noreply.github.com>
2025-05-22 21:01:08 +0800

    Initial Commit

---
+++ appuninstall/appuninstallApp.swift
@@ -0,0 +1,17 @@
+//
+//  appuninstallApp.swift
+//  appuninstall
+//
+//  Created by Jamie Huang on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct appuninstallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
